1ec3ac766a8ceecdf5590e5e4bfd8d3401dbb1f9
Paul de Rosanbo <paul@rosanbo.com>
2016-09-19 09:16:16 +0200

    jshint

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const jsdom = require('jsdom');
+
 
 /**
  * @param {DOMDocument} document
@@ -11,7 +15,7 @@ function createSections(document, article) {
 
     nodes.forEach(function (node) {
         if (/^h[1-6]$/i.test(node.nodeName)) {
-            section = document.createElement("section");
+            section = document.createElement('section');
             article.appendChild(section);
         }
         section.appendChild(node);
@@ -19,3 +23,40 @@ function createSections(document, article) {
 
     return article;
 }
+
+
+/**
+ * hsplit a html document from a string
+ *
+ * @param {String} html
+ * @param {String} rootNodeName optional root nodeName, default is article
+ * @return {Promise}
+ */
+function hsplitDocument(html, rootNodeName) {
+    if (!rootNodeName) {
+        rootNodeName = 'article';
+    }
+
+    return new Promise((resolve, reject) => {
+
+        jsdom.env(
+            html,
+            [],
+            (err, window) => {
+                if (err) {
+                    return reject(err);
+                }
+                let document = window.document;
+                let article = document.getElementsByTagName(rootNodeName)[0];
+                createSections(window.document, article);
+            }
+        );
+
+    });
+}
+
+
+
+exports = module.exports = {
+    document: hsplitDocument
+};
